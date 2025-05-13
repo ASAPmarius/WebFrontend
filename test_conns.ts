518daeefb6b516af3d2bd272579f6fdb6a6a3859
ASAP Marius <marius.poirierpro@gmail.com>
2025-05-13 16:36:10 +0200

    rm backup + test_conns

--- test_conns.ts
+++
@@ -1,7 +0,0 @@
-import { connect } from "https://deno.land/std@0.170.0/io/util.ts";
-const host = Deno.env.get("DB_HOST")!.trim();
-const port = Number(Deno.env.get("DB_PORT"));
-console.log("→ connecting to", JSON.stringify(host), port);
-const conn = await Deno.connect({ hostname: host, port });
-console.log("✅ connected!");
-conn.close();
